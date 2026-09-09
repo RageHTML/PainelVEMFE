3754ac774f597ef79f8d3baaa4df777611c20c02
RageHTML <deyvidm24@gmail.com>
2026-09-09 21:46:54 +0000

    fix: bat do windows

--- vemfestejar.bat
+++ vemfestejar.bat
@@ -1,9 +1,21 @@
 @echo off
+NET FILE >nul 2>&1
+if '%errorlevel%' == '0' ( goto gotAdmin ) else ( goto getAdmin )
+:getAdmin
+if '%1'=='ELEV' (shift & goto gotAdmin)
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "ELEV", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+cd /d "%~dp0"
+@echo off
 
 cd /d "%~dp0"
 
-start "" /B cmd /c "timeout /t 3 /nobreak >nul && start chrome http://127.0.0.1:8000/clientes"
+start "" /B cmd /c "timeout /t 5 /nobreak >nul && start chrome http://127.0.0.1:8000/clientes"
 
-uv run fastapi run main.py --port 8000
+uv run uvicorn main:app --reload --port 8000
 
 pause
